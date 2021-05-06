d63f087dbb7da2d0585db3f40bf84055bdae534b
solehudinMQ <solehudinmq@gmail.com>
2021-05-07 05:38:39 +0700

    debug

--- lib/jojo_encrypted/mongoid/attr_encrypted.rb
+++ lib/jojo_encrypted/mongoid/attr_encrypted.rb
@@ -10,9 +10,11 @@ module JojoEncrypted
               return unless value
 
               self[attribute] = JojoEncrypted::Mongoid::Services::EncryptionService.encrypt(value)
+              self["#{attribute}_decrypted".to_sym] = value
             end
 
             define_method(attribute) do
+              byebug
               self["#{attribute}_encrypted".to_sym] = self[attribute]
               self[attribute] = JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(self[attribute])
             end
