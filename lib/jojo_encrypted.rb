c4d5e8447aca96b4a164ac65c993c7a3963b4290
solehudinMQ <solehudinmq@gmail.com>
2021-05-06 23:55:45 +0700

    fix read yml

--- lib/jojo_encrypted.rb
+++ lib/jojo_encrypted.rb
@@ -1,5 +1,3 @@
-require 'yaml'
-
 require "jojo_encrypted/version"
 require "jojo_encrypted/mongoid/services/encryption_service"
 require "jojo_encrypted/mongoid/attr_encrypted"
@@ -7,8 +5,4 @@ require "jojo_encrypted/mongoid/attr_encrypted"
 module JojoEncrypted
   class Error < StandardError; end
   # Your code goes here... 
-
-  YAML.load_file("config/jojo_encrypted.yml").each do |key, value|
-    ENV[key.to_s] = value
-  end
 end
