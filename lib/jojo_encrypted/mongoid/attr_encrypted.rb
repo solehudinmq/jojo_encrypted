86399c269f54e8d11c86bc432676cd3a7b35a7e9
solehudinMQ <solehudinmq@gmail.com>
2021-05-07 04:41:32 +0700

    debug

--- lib/jojo_encrypted/mongoid/attr_encrypted.rb
+++ lib/jojo_encrypted/mongoid/attr_encrypted.rb
@@ -6,30 +6,26 @@ module JojoEncrypted
       class_methods do
         def encrypts(*attributes)
           attributes.each do |attribute|
-            # define_method("#{attribute}=".to_sym) do |value|
-            #   return if value.nil?
+            define_method("#{attribute}=".to_sym) do |value|
+              return if value.nil?
               
-            #   self.public_send(
-            #     "encrypted_#{attribute}=".to_sym,
-            #     JojoEncrypted::Mongoid::Services::EncryptionService.encrypt(value)
-            #   )
-            # end
+              puts "SETTER ATTR : #{attribute}"
+              puts "SETTER VALUE : #{value}"
+              puts "SETTER SELF : #{self}"
 
-            # define_method(attribute) do
-            #   value = self.public_send("encrypted_#{attribute}".to_sym)
-            #   JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(value) if value.present?
-            # end
-
-            puts "SELF : #{self}"
-            puts "ATTR : #{attribute}"
-            puts "VALUE : #{self[attribute]}"
-
-            define_method("#{attribute}") do
-              self[attribute]=JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(self[attribute])
+              self.public_send(
+                "encrypted_#{attribute}=".to_sym,
+                JojoEncrypted::Mongoid::Services::EncryptionService.encrypt(value)
+              )
             end
 
-            define_method("#{attribute}_encrypted") do
-              self[:"#{attribute}_encrypted"]=JojoEncrypted::Mongoid::Services::EncryptionService.encrypt(self[attribute])
+            define_method(attribute) do
+              value = self.public_send("encrypted_#{attribute}".to_sym)
+
+              puts "GETTER ATTR : #{attribute}"
+              puts "GETTER VALUE : #{value}"
+
+              JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(value) if value.present?
             end
           end
         end
