8f60424f3eeefae5a6e1598863d6888dd00d1fc1
solehudinMQ <solehudinmq@gmail.com>
2021-05-10 03:50:32 +0700

    try to masking

--- lib/jojo_encrypted/mongoid/attr_encrypted.rb
+++ lib/jojo_encrypted/mongoid/attr_encrypted.rb
@@ -14,15 +14,49 @@ module JojoEncrypted
               self[attribute] = JojoEncrypted::Mongoid::Services::EncryptionService.encrypt(value)
             end
 
-            define_method(attribute) do
-              self[attribute] = self[attribute]
-            end
+            # define_method(attribute) do
+            #   self[attribute] = self[attribute]
+            # end
 
-            define_method("#{attribute}_decrypted") do
-              self["#{attribute}_decrypted".to_sym] = JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(self[attribute])
-            end
+            # unless self.new_record?
+            #   define_method("#{attribute}_decrypted") do
+            #     self["#{attribute}_decrypted".to_sym] = JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(self[attribute])
+            #   end
+            # end
+
+            masking_fields(self, attribute)
           end
         end
+
+        # encrypts fields as temporary (encryptions is not save to db)
+        def temp_encrypts(*attributes)
+          attributes.each do |attribute|
+            # define_method(attribute) do
+            #   self[attribute] = self[attribute]
+            # end
+
+            # unless self.new_record?
+            #   define_method("#{attribute}_decrypted") do
+            #     self["#{attribute}_decrypted".to_sym] = JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(self[attribute])
+            #   end
+            # end
+
+            masking_fields(self, attribute)
+          end
+        end
+
+        private
+          def masking_fields(object_masking, attribute)
+            define_method(attribute) do
+              object_masking[attribute] = object_masking[attribute]
+            end
+
+            unless object_masking.new_record?
+              define_method("#{attribute}_decrypted") do
+              object_masking["#{attribute}_decrypted".to_sym] = JojoEncrypted::Mongoid::Services::EncryptionService.decrypt(object_masking[attribute])
+              end
+            end
+          end
       end
     end
   end
